2fd17f4ad0e799150c13d719b72da31fda46dd5e
japancakes <japancakes@live.com>
2019-11-26 00:17:05 -0800

    Fixed RequestFragment

--- app/src/main/java/com/example/littyfy_v0/RequestsFragment.java
+++ app/src/main/java/com/example/littyfy_v0/RequestsFragment.java
@@ -111,7 +111,7 @@ public class RequestsFragment extends Fragment {
                             public void onClick(View view) {
                                 String songKey = songReqRef.child("Queue").push().getKey();
                                 HashMap<String, Object> songInfoMap = new HashMap<>();
-                                songInfoMap.put("title", dj);
+                                songInfoMap.put("title", songInfo.getTitle());
                                 songInfoMap.put("artist", songInfo.getArtist());
                                 songReqRef.child("Queue").child(songKey).updateChildren(songInfoMap);
 
